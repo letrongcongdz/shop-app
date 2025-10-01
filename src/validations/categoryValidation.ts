ba0d53b6fd71444d21b81faed40f8b01c0c9b2cc
letrongcongdz <cong.letrong@ncc.asia>
2025-10-01 10:50:48 +0700

    feat(validation): Add feature validation

--- src/validations/categoryValidation.ts
+++ src/validations/categoryValidation.ts
@@ -1,9 +1,13 @@
 import { z } from "zod";
 
 export const createCategorySchema = z.object({
-  name: z.string().min(2, "Category name must be at least 2 characters").max(100, "The name is too long"),
+  name: z.string()
+  .min(2, "Category name must be at least 2 characters")
+  .max(100, "The name is too long"),
 });
 
 export const updateCategorySchema = z.object({
-  name: z.string().min(2, "Category name must be at least 2 characters").max(100, "The name is too long"),
+  name: z.string()
+  .min(2, "Category name must be at least 2 characters")
+  .max(100, "The name is too long"),
 });
